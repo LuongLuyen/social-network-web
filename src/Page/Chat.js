65e4b405978db688bd039127ac77418c5400cf87
LuongLuyen <luongvanluyen2003@gmail.com>
2023-07-29 11:19:24 +0700

    component chat nav,chat profile

--- src/Page/Chat.js
+++ src/Page/Chat.js
@@ -3,6 +3,9 @@ import Header from "../Components/Header"
 import ChatList from "../Components/ChatList"
 import { BsMessenger } from 'react-icons/bs'
 import { BsPeopleFill } from 'react-icons/bs'
+import { CgProfile } from 'react-icons/cg'
+import { MdOutlineNotifications } from 'react-icons/md'
+import { BiSearchAlt } from 'react-icons/bi'
 import Avata from "../assets/img/aaa.jpg"
 function Chat() {
     return ( 
@@ -23,7 +26,30 @@ function Chat() {
                 <ChatList/>
                 <div className="chat__main">main</div>
 
-                <div className="chat__profile">profile</div>
+                <div className="chat__profile">
+                    <div className="chat__profile-user">
+                        <img className="chat__profile-avt" alt="/" src= {Avata}/>
+                        <div className="chat__profile-name">Nguyễn Văn A</div>
+                        <div className="chat__profile-active">Đang hoạt động</div>
+                    </div>
+                    <div>
+                        <span className="chat__profile-icon">
+                            <CgProfile/>
+                        </span>
+                        <span className="chat__profile-icon">
+                            <MdOutlineNotifications/>
+                        </span>
+                        <span className="chat__profile-icon">
+                            <BiSearchAlt/>
+                        </span>
+                    </div>
+                    <div className="Chat__profile-f">
+                        <div className="Chat__profile-function">Thông tin về đoạn chat</div>
+                        <div className="Chat__profile-function">Tùy chỉnh đoạn chat</div>
+                        <div className="Chat__profile-function">File phương tiện, file và liên kết</div>
+                        <div className="Chat__profile-function">Quyền riêng tư & hỗ trợ</div>
+                    </div>
+                </div>
             </div>
         </>
      )
