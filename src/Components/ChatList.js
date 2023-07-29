00aaa90bd0342b54553bd09c169e13d6cb658580
LuongLuyen <luongvanluyen2003@gmail.com>
2023-07-29 09:10:51 +0700

    Component ChatList

---
+++ src/Components/ChatList.js
@@ -0,0 +1,96 @@
+import { BiSolidVideoPlus } from 'react-icons/bi'
+import { HiPencilAlt } from 'react-icons/hi'
+import Avata from "../assets/img/aaa.jpg"
+import "../Style/ComponentStyle/ChatList.css"
+function ChatList() {
+    return ( 
+        <div className="chat__list">
+            <div className="chat__list-header">
+                <div className="chat__list-title">Chat</div>
+                <div>
+                    <span className="chat__list-icon"><BiSolidVideoPlus/></span>
+                    <span className="chat__list-icon"><HiPencilAlt/></span>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+            <div className="chat__list-user">
+                <img className="chat__list-avt" alt="/" src ={Avata}/>
+                <div>
+                    <div className="chat__list-name">Nguyễn Văn A</div>
+                    <div className="chat__list-d">Bạn: toi la yasuo</div>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default ChatList;
